b596f3a5d1c56a37ca721455a8bb40bd350a3503
iiuhenio <iuheniomail@yandex.ru>
2022-10-07 16:29:58 +0300

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,75 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.*;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+/**
+ * 1. Конфигурирование
+ * Начало работы происходит с создания класса управляющего всеми работами.
+ * В объект Scheduler мы будем добавлять задачи, которые хотим выполнять периодически.
+ * 2. Создание задачи.
+ * 3. Создание расписания
+ * Конструкция ниже настраивает периодичность запуска.
+ * В нашем случае, мы будем запускать задачу через 10 секунд и делать это бесконечно.
+ * 4. Задача выполняется через триггер
+ * Здесь можно указать, когда начать запуск.
+ * Мы хотим сделать это сразу
+ * 5. Загрузка задачи и триггера в планировщик
+ */
+public class AlertRabbit {
+    public static void main(String[] args) {
+        try {
+
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(AlertRabbit.getInterval())
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * (2)
+     * quartz каждый раз создает объект с типом org.quartz.Job.
+     * Вам нужно создать класс реализующий этот интерфейс.
+     * Внутри этого класса нужно описать требуемые действия.
+     * В нашем случае - это вывод на консоль текста.
+     */
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    /**
+     * файл, который хранит свойства нашего проекта
+     * создаем объект Properties и загружаем в него данные из файла.
+     * получаем значения свойств из объекта Properties
+     * получаем числовое значение
+     */
+    public static int getInterval() throws IOException {
+        File file = new File("rabbit.properties");
+        Properties properties = new Properties();
+        properties.load(new FileReader(file));
+        String interval = properties.getProperty("rabbit.interval");
+        return Integer.parseInt(interval);
+    }
+}
