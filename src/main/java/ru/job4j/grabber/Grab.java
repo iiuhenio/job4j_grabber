205a8ca8afaed52d9112175b4392ace7b256cb96
iiuhenio <iuheniomail@yandex.ru>
2022-10-18 12:16:47 +0300

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

---
+++ src/main/java/ru/job4j/grabber/Grab.java
@@ -0,0 +1,13 @@
+package ru.job4j.grabber;
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+
+/**
+ * В этом проекты мы будем использовать quartz для запуска парсера.
+ * Но напрямую мы не будем его использовать.
+ * Абстрагируемся через интерфейс.
+ */
+public interface Grab {
+    void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException;
+}
---
+++ src/main/java/ru/job4j/grabber/Store.java
@@ -0,0 +1,17 @@
+package ru.job4j.grabber;
+
+import java.util.List;
+
+/**
+ * Наш проект будет хранить данные в базе Postgresql. Связь с базой будет осуществляться через интерфейс.
+ * ru.job4j.grabber.Store.
+ *
+ * Метод save() позволяет сохранить объявление в базе
+ * Метод getAll() позволяет вызвать объявление из базы
+ * Метод findBy() позволяет извлечь объявление из базы по id
+ */
+public interface Store {
+    void save(Post post);
+    List<Post> getAll();
+    Post findBy(int id);
+}
