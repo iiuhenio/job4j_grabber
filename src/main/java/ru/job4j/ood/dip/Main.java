7ec782d4dffd99bf3321347e72eee0b4323922c7
iiuhenio <iuheniomail@yandex.ru>
2022-12-15 11:20:52 +0300

    0. Принцип инверсии зависимостей [#4917]

---
+++ src/main/java/ru/job4j/ood/dip/Main.java
@@ -0,0 +1,29 @@
+package ru.job4j.ood.dip;
+
+import java.util.function.Consumer;
+
+/**
+ * Здесь проблема в том, что например, метод main() использует класс DataAcess,
+ * тот в свою очередь обращается к классу Client итд
+ * В соответствии с принципом DIP должно быть разделение на верхние классы и нижние классы
+ * и они не должны знать друг о друге.
+ * Нужно было бы сделать промежуточные интерфейс, который бы взаимодействовал между уровнями
+ */
+public class Main {
+    public static void main(String[] args) {
+        Consumer<DataAccess> execute = DataAccess::execute;
+    }
+}
+
+class DataAccess {
+    void execute() {
+        System.out.println("execute");
+    }
+}
+
+class Client {
+    DataAccess da = new DataAccess();
+    void doJob() {
+        da.execute();
+    }
+}
