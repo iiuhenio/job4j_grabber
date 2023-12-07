70147c95db911639cc634e7ae157c9440cd654c7
iiuhenio <iuheniomail@yandex.ru>
2023-12-07 16:55:56 +0300

    1.1. Job c параметрами [#260360 #464901]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,12 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -29,23 +35,37 @@ import static org.quartz.SimpleScheduleBuilder.*;
  * 5. Загрузка задачи и триггера в планировщик
  */
 public class AlertRabbit {
-    public static void main(String[] args) {
-        try {
 
+    public static Connection initConnection() throws Exception {
+        Class.forName(getProperties().getProperty("driver"));
+        String url = getProperties().getProperty("url");
+        String login = getProperties().getProperty("login");
+        String password = getProperties().getProperty("password");
+        return DriverManager.getConnection(url, login, password);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+            List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", initConnection());
+
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(AlertRabbit.genProperties().getProperty("rabbit.interval")))
+                    .withIntervalInSeconds(Integer.parseInt(AlertRabbit.getProperties().getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
-        }
+            Thread.sleep(5000);
+            scheduler.shutdown();
+            System.out.println(store);
     }
 
     /**
@@ -56,9 +76,25 @@ public class AlertRabbit {
      * В нашем случае - это вывод на консоль текста.
      */
     public static class Rabbit implements Job {
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
+            Long l = System.currentTimeMillis();
+            store.add(l);
+            String sql = "INSERT INTO rabbit (created_date) VALUES (?)";
+            try {
+                PreparedStatement preparedStatement = initConnection().prepareStatement(sql);
+                preparedStatement.setLong(1, l);
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -68,8 +104,7 @@ public class AlertRabbit {
      * получаем значения свойств из объекта Properties
      * получаем числовое значение
      */
-
-    public static Properties genProperties() {
+    public static Properties getProperties() {
         Properties config = new Properties();
         try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
             config.load(in);
@@ -78,4 +113,6 @@ public class AlertRabbit {
         }
         return config;
     }
+
+
 }
